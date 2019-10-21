2e997a68998fee85ad56ef40dc93afc7b3ffc2b6
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-10-21 11:46:47 +0300

    Add project files.

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        int x, y ;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            x = Convert.ToInt16(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            y = Convert.ToInt16(textBox2.Text);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Location = new System.Drawing.Point(x, y);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
